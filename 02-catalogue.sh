2bccb2ed7f4a7b96367d1657fff52d8042a250a3
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-16 23:37:31 +0530

    mongodb app

--- 02-catalogue.sh
+++ 02-catalogue.sh
@@ -49,7 +49,7 @@ if [ $? -ne 0 ]
     useradd roboshop
     validate $? "roboshop user created"
     else
-    echo "user alreay exists $Y skipped"
+    echo "user alreay exists $Y skipped $N"
 fi
 
 mkdir -p /app &>> $LOGFILE
