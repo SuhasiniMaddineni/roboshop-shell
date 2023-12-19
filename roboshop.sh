dd094347eab837fe45a9cc5bd9d2a14dbe4bbaa4
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-19 22:43:50 +0530

    instances_creation

---
+++ roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+AMI=ami-03265a0778a880afb
+SG_ID=sg-0ab4941a3f9f404b7
+
+INSTANCES=("mongodb" "mysql" "shipping" "redis" "rabbitmq" "user" "cart" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+    INSTANCE_TYPE="t3.small"
+    else
+    INSTANCE_TYPE="t2.micro"
+    fi 
+    aws ec2 run-instances --image-id ami-03265a0778a880afb 
+    --instance-type $INSTANCE_TYPE --security-group-ids sg-0ab4941a3f9f404b7
+done
+
