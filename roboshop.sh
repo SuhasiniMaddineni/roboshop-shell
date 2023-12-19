fcb593a804bd749d0b0617de8da32cdb7278e886
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-19 22:47:11 +0530

    instances_creation

--- roboshop.sh
+++ roboshop.sh
@@ -8,13 +8,15 @@ INSTANCES=("mongodb" "mysql" "shipping" "redis" "rabbitmq" "user" "cart" "paymen
 
 for i in "${INSTANCES[@]}"
 do
+    echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
     INSTANCE_TYPE="t3.small"
     else
     INSTANCE_TYPE="t2.micro"
     fi 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb 
-    --instance-type $INSTANCE_TYPE --security-group-ids sg-0ab4941a3f9f404b7
+
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE 
+    --security-group-ids sg-0ab4941a3f9f404b7
 done
 
