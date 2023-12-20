50d742bf5cf45b0bd427968f87353be9f913de91
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-20 13:39:15 +0530

    instances_route53

--- instance_route53.sh
+++ instance_route53.sh
@@ -7,7 +7,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance name is : $i"
+    
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -15,6 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0ab4941a3f9f404b7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0ab4941a3f9f404b7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" alue=$i}]" alue=$i}]" --query 'Instances[0].PrivateIpAddress' --output text) 
+    echo "$i: $IP_ADDRESS"
 done
 
