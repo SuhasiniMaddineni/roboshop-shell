90c1f38277614b85a77d8d8afaa52eb1090870b2
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-20 11:38:50 +0530

    instances_route53

--- instance_route53.sh
+++ instance_route53.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-0ab4941a3f9f404b7 #replace with your SG ID
+SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 
 for i in "${INSTANCES[@]}"
 do
-
-    echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -16,6 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE 
-    --security-group-ids sg-0ab4941a3f9f404b7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
 done
+
