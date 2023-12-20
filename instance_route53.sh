bcf1f6f4208d5f8d40a881f464d685e08816c362
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-20 11:59:59 +0530

    instances_route53

--- instance_route53.sh
+++ instance_route53.sh
@@ -7,6 +7,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
+    echo "instance name is : $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -15,7 +16,7 @@ do
     fi
 
     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE 
-    --security-group-ids sg-087e7afb3a936fce7 
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+    --security-group-ids sg-087e7afb3a936fce7 --tags Key=Name,Value=$i
+    #--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
 done
 
